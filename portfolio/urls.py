2f16bf04b0124791f1b17b2b2578938c63268b29
Shiny <shinyshiny966@gmail.com>
2026-06-06 14:35:11 +0530

    ayshjjghj

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,7 +2,8 @@
 URL configuration for portfolio project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.django
+    .com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
