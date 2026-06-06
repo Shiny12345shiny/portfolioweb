90cab670a1fd4a9789bffa512a6a1b8d23485b17
Shiny <shinyshiny966@gmail.com>
2026-06-06 11:01:16 +0530

    initisalportfoloi

---
+++ main/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Project(models.Model):
+    title=models.CharField(max_length=100)
+    description=models.TextField()
+    image=models.ImageField(upload_to='projects/default.jpg')
+    github=models.URLField()
+
+    def __str__(self):
+        return self.title
